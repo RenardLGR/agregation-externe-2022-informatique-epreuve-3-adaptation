058f4477b2be771e45f97f1ade26ab48238e4147
Olivier Reynard <olivier.reynard17@gmail.com>
2023-02-12 17:32:34 +0100

    question 26

--- src/partie-2/main.js
+++ src/partie-2/main.js
@@ -1,3 +1,7 @@
+let putQueenButton = document.getElementById("putQueen")
+putQueenButton.addEventListener('click', putQueen)
+
+//Initialize
 drawBoard()
 
 function drawBoard(){
@@ -10,11 +14,20 @@ function drawBoard(){
         for(let j=0 ; j<8 ; j++){
             let td = document.createElement('td')
             td.classList.add(isWhite ? 'white' : 'black')
-            td.classList.add(letters[j]+i)
+            td.id = letters[j]+i
             isWhite = !isWhite
             row.appendChild(td)
         }
         isWhite = !isWhite
         board.appendChild(row)
     }
+}
+
+function putQueen(){
+    let coord = document.getElementById("square").value
+    let targetSquare = document.getElementById(coord)
+    let img = document.createElement('img')
+    img.src = "assets/img/queen.png"
+    img.alt = "queen"
+    targetSquare.appendChild(img)
 }
